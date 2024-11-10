43debb0b213f84e5595586428fbff652498cd310
senhas-rgb <senhasdiwan@gmail.com>
2024-11-10 20:52:26 +0530

    implementation of recent files in dashbaord

--- src/popup/popup.js
+++ src/popup/popup.js
@@ -3,6 +3,20 @@ console.log("Popup.js loaded");
 const zapButton = document.querySelector("button#zap");
 const saveButton = document.querySelector("button#save");
 const dashboardButton = document.querySelector("button#dashboard");
+const searchInput = document.querySelector("input#search");
+const recentFiles = document.querySelector("div#recentFiles");
+const openButton = document.querySelector("button#open");
+const deleteButton = document.querySelector("button#delete");
+const searchButton = document.querySelector("button#search");
+
+function search() {
+    console.log("Searching for files");
+    const searchTerm = document.getElementById("search").value;
+    console.log(searchTerm);
+    const files = document.querySelectorAll("div.file");
+    console.log(files);
+}
+
 
 function zap() {
     console.log("Zap button clicked");
@@ -20,4 +34,3 @@ function dashboard() {
 zapButton.addEventListener("click", zap);
 saveButton.addEventListener("click", save);
 dashboardButton.addEventListener("click", dashboard);
-
