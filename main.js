96b8209eb87ad13a3dd722bace30e9818695e806
senhas-rgb <senhasdiwan@gmail.com>
2024-11-12 15:47:49 +0530

    bug fixes

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
 // defining all the elements
 const DashboardButton = document.getElementById("DashboardButton");
+const body = document.querySelector("body");
+const darkMode = document.getElementById("darkMode");
 
 
 // defining all the functions
@@ -7,5 +9,10 @@ function openNewTabDashboard() {
   window.open("dashboard.html", "_blank");
 }
 
+function changeTheme() {
+  body.dataset.theme = body.dataset.theme === "dark" ? "light" : "dark";
+}
+
 // adding the event listeners
 DashboardButton.addEventListener("click", openNewTabDashboard);
+darkMode.addEventListener("click", changeTheme);
