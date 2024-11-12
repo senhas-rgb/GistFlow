ccd2125265c55f7ddc945e29d06fcc87bf89bea3
senhas-rgb <senhasdiwan@gmail.com>
2024-11-12 15:44:59 +0530

    css variable declaration

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 // defining all the elements
 const DashboardButton = document.getElementById("DashboardButton");
 
+
 // defining all the functions
 function openNewTabDashboard() {
   window.open("dashboard.html", "_blank");
