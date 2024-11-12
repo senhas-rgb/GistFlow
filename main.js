d51d1772acd54c4e60afd8f178318a4612526880
senhas-rgb <senhasdiwan@gmail.com>
2024-11-12 15:11:34 +0530

    Initialising a confirmation dialog

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ const DashboardButton = document.getElementById("DashboardButton");
 
 // defining all the functions
 function openNewTabDashboard() {
-    window.open("dashboard.html", "_blank");
+  window.open("dashboard.html", "_blank");
 }
 
 // adding the event listeners
