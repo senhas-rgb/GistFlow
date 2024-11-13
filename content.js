63e1d474c570e38d41f063b021bb095d95bea4e3
Sadeve Methmika <sadevemdev@gmail.com>
2024-11-13 20:53:39 +0530

    Removed audio stuff in content.js file

--- content.js
+++ content.js
@@ -2,22 +2,15 @@ const navBtns = document.querySelectorAll(".nav-btn");
 const zapBtns = document.querySelectorAll(".zap-btn")
 const textInterface = document.querySelector(".text");
 const videoInterface = document.querySelector(".video");
-const audioInterface = document.querySelector(".audio");
 
 for (let button of navBtns) {
     button.addEventListener("click", ()=>{
         if (button.classList.contains("text-sect")) {
             textInterface.style.display = "block";
             videoInterface.style.display = "none";
-            audioInterface.style.display = "none";
         } else if (button.classList.contains("vid-sect")) {
             textInterface.style.display = "none";
             videoInterface.style.display = "block";
-            audioInterface.style.display = "none";
-        } else if (button.classList.contains("audio-sect")) {
-            textInterface.style.display = "none";
-            videoInterface.style.display = "none";
-            audioInterface.style.display = "block";
         }
     })
 }
@@ -27,8 +20,6 @@ for (let button of zapBtns) {
         if (button.classList.contains("text-sect")) {
             ScrapeText();
         } else if (button.classList.contains("vid-sect")) {
-
-        } else if (button.classList.contains("audio-sect")) {
 
         }
     })
