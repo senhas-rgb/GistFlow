44adde2aaa0478ca944db4c33b8085b31ff0a360
Sadeve Methmika <sadevemdev@gmail.com>
2024-11-13 17:01:10 +0530

    Added functionality so that when the navbar buttons are clicked, display changes

---
+++ content.js
@@ -0,0 +1,22 @@
+const zapBtns = document.querySelectorAll(".zap-btn");
+const textInterface = document.querySelector(".text");
+const videoInterface = document.querySelector(".video");
+const audioInterface = document.querySelector(".audio");
+
+for (let button of zapBtns) {
+    button.addEventListener("click", ()=>{
+        if (button.classList.contains("text-sect")) {
+            textInterface.style.display = "block";
+            videoInterface.style.display = "none";
+            audioInterface.style.display = "none";
+        } else if (button.classList.contains("vid-sect")) {
+            textInterface.style.display = "none";
+            videoInterface.style.display = "block";
+            audioInterface.style.display = "none";
+        } else if (button.classList.contains("audio-sect")) {
+            textInterface.style.display = "none";
+            videoInterface.style.display = "none";
+            audioInterface.style.display = "block";
+        }
+    })
+}
