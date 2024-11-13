45b0c0536a815f56d43fc0f5f1881ecc83f94d96
Sadeve Methmika <sadevemdev@gmail.com>
2024-11-13 16:25:58 +0530

    Made controls to the nav bar

---
+++ nav-animations.js
@@ -0,0 +1,37 @@
+const slider = document.querySelector(".selector");
+const buttons = document.querySelectorAll("P");
+let sliderOffset = slider.offsetLeft;
+let offset;
+let id;
+
+for (let button of buttons) {
+    clearInterval(id);
+    button.addEventListener("click", ()=>{
+        if (button.innerHTML == "Text") {
+            offset = button.offsetLeft - 23;
+            slider.style.left = `${offset}px`;
+        } else if (button.innerHTML == "Video") {
+            offset = button.offsetLeft -15;
+            slider.style.left = `${offset}px`;
+        } else if (button.innerHTML == "Audio") {
+            offset = button.offsetLeft - 8;
+            slider.style.left = `${offset}px`;
+        }
+    })
+}
+
+function frame(offset) {
+    if (offset == sliderOffset) {
+        clearInterval(id);
+      } else {
+        while (sliderOffset != offset){
+            if (offset > sliderOffset) {
+                sliderOffset++;
+                slider.style.left = sliderOffset + "px"
+            } else if (offset < sliderOffset) {
+                sliderOffset--;
+                slider.style.left = sliderOffset + "px"
+            }
+        }
+    }
+}
