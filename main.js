fed0f386dfcbfcadbebe540518418505d296c39e
senhas-rgb <senhasdiwan@gmail.com>
2024-11-13 21:14:00 +0530

    Clean up

--- main.js
+++ main.js
@@ -1,20 +1,12 @@
 // defining all the elements
 const DashboardButton = document.getElementById("DashboardButton");
 const body = document.querySelector("body");
-const darkMode = document.getElementById("darkMode");
-const API = "AIzaSyDxhLMv7vUFmzvyY99k7UlVK1kC-798e24";
-
+const DarkModeBtn = document.getElementById("dark-mode-btn");
 // defining all the functions
 function openNewTabDashboard() {
   window.open("dashboard.html", "_blank");
 }
 
-function changeTheme() {
-  body.dataset.theme = body.dataset.theme === "dark" ? "light" : "dark";
-}
-
-
-
 // adding the event listeners
 DashboardButton.addEventListener("click", openNewTabDashboard);
-darkMode.addEventListener("click", changeTheme);
+DarkModeBtn.addEventListener("click", changeTheme);
