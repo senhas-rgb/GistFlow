db711b86bb8f5cbbf7bb05d2df35f54e14169b7f
Sadeve Methmika <sadevemdev@gmail.com>
2024-11-14 11:10:48 +0530

    Added file name changing functionality

--- file-name-edit.js
+++ file-name-edit.js
@@ -1,12 +1,26 @@
 const editBtns = document.querySelectorAll(".title-edit-btn");
+const inputFields = document.querySelectorAll(".file-name")
 
 for (let button of editBtns) {
     button.addEventListener("click", ()=> {
         const fileNameInput = button.parentElement.querySelector(".file-name")
         fileNameInput.classList.add("file-name-edit");
-        fileNameInput.attributes.removeNamedItem("disabled")
-        fileNameInput.value = fileNameInput.attributes.getNamedItem("placeholder").textContent;
+        fileNameInput.removeAttribute("disabled")
         fileNameInput.focus();
+        let temp = fileNameInput.value;
+        fileNameInput.value = "";
+        fileNameInput.value = temp
     }) 
+}
+
+for (let input of inputFields) {
+    input.addEventListener("keydown", (event)=>{
+        if (!input.hasAttribute("disabled")) {
+            if (event.key === 'Enter') {
+                input.setAttribute("disabled", "true");
+                input.classList.remove("file-name-edit")
+            }
+        }
+    })
 }
 
